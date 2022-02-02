9f32bea67c307a53252d4663fb8b6e6982a8faa3
vbalaji215-dev <vbalaji215@outlook.com>
2022-02-02 16:00:49 -0500

    Initial Add

---
+++ src/main/java/com/brihaspathee/quotes/MotivationalQuote.java
@@ -0,0 +1,17 @@
+package com.brihaspathee.quotes;
+
+/**
+ * Created in Intellij IDEA
+ * User: Balaji Varadharajan
+ * Date: 02, February 2022
+ * Time: 3:48 PM
+ * Project: quotes
+ * Package Name: com.brihaspathee.quotes
+ * To change this template use File | Settings | File and Code Template
+ */
+public class MotivationalQuote {
+
+    public String getMotivationQuote(){
+        return "Do your best";
+    }
+}
